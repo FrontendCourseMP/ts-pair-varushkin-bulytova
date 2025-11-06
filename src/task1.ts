27b9b03522fea977eba5abcd114ebc5aa3575d5b
mvbulytova <bulytova.m@yandex.ru>
2025-11-06 16:02:34 +0300

    pr1

---
+++ src/task1.ts
@@ -0,0 +1,24 @@
+const nameInput = document.getElementById('name') as HTMLInputElement;
+const lastnameInput = document.getElementById('lastname') as HTMLInputElement;
+const patronymicInput = document.getElementById('patronymic') as HTMLInputElement;
+const button = document.getElementById('button');
+const output = document.getElementById('output');
+
+function getFIO() {
+    const name = nameInput.value;
+    const lastname = lastnameInput.value;
+    const patronymic = patronymicInput.value;
+    
+    if (name && lastname && patronymic) {
+        const FIO = `${lastname} ${name[0]}. ${patronymic[0]}.`;
+        if (output) {
+            output.textContent = FIO;
+        }
+    } else {
+        if (output) {
+            output.textContent = 'Заполните все поля';
+        }
+    }
+}
+
+button?.addEventListener('click', getFIO);
